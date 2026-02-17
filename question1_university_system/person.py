3a12062385b641fb99a44655a903474b15141be0
Amalka <COMScDS252P-010@student.nibm.lk>
2026-02-17 09:16:17 +0530

    person created

---
+++ question1_university_system/person.py
@@ -0,0 +1,15 @@
+class Person:
+    def __init__(self, name, person_id, email, phone):
+        self.name = name
+        self.person_id = person_id
+        self.email = email
+        self.phone = phone
+
+    def get_info(self):
+        ''' returns the public infromation about the person'''
+        print(f'{self.name} is a memeber of University')
+
+    def update_info(self, email, phone):
+        '''update person info'''
+        self.email = email
+        self.phone = phone
